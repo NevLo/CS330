9e6aa669e8b987c6126cd1fd0510a1f63279c472
NevLo <pilleychristian@gmail.com>
2021-09-22 16:52:30 -0700

    random additions to main

--- CS330HW2/src/Main.java
+++ CS330HW2/src/Main.java
@@ -15,13 +15,23 @@ public class Main {
 		mytree = new Ruffles(mytree);
 		mytree = new LEDS(mytree);
 		printtree(mytree);
-		
 		//EXPECTED OUTPUT:
 		//Tree already has a star!
 		//Blue Spruce tree decorated with, a Star, Ruffles, Ruffles costs $26.00
+		Tree tree2 = new FraserFir();
+		tree2 = new Star(tree2);
+		tree2 = new Star(tree2);
+		tree2 = new BallsBlue(tree2);
+		tree2 = new BallsRed(tree2);
+		tree2 = new BallsSilver(tree2);
+		tree2 = new Ribbons(tree2);
+		tree2 = new Ruffles(tree2);
+		tree2 = new LEDS(tree2);
+		tree2 = new Lights(tree2);
+		printtree(tree2);
 		
-		
-		
+		tree2 = new BalsamFir();
+		printtree(tree2);
 	}
 
 	private static void printtree(Tree mytree) {
